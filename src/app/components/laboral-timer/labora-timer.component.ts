d2fa5e9c402a1f81f6dd48c15b0da46567e79be2
Adrián Pisabarro García <apisabarro@teknei.com>
2024-02-24 01:20:19 +0100

    Update angular.json and laboral-timer.component.ts

--- src/app/components/laboral-timer/labora-timer.component.ts
+++ src/app/components/laboral-timer/labora-timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DateTimeComponent } from '../date-time/date-time.component';
 import { CommonModule } from '@angular/common';
@@ -17,27 +17,25 @@ export enum States {
   templateUrl: './laboral-timer.component.html',
   styleUrl: './laboral-timer.component.css'
 })
-export class LaboralTimerComponent implements OnInit{
+
+export class LaboralTimerComponent{
 
   time:number = 0;
   timer: string = "00:00:00"
-
-  constructor() {
-    console.log('constructor');
-    console.log(States.Start);
-   }
-
-  ngOnInit() {
-    console.log('init');
+  displayButtons = {
+    start: true,
+    stop: false,
+    pause: false,
+    reset: false
   }
 
   start() {
     setInterval(() => {
       this.time++;
       this.timer = this.formatTime(this.time);
-    }, 1000); // 1000 milisegundos = 1 segundo
-
-    console.log('start');
+    }, 1000);
+    this.saveCurrentDateTime();
+    this.displayButtons.start = false;
   }
 
   stop() {
@@ -48,27 +46,21 @@ export class LaboralTimerComponent implements OnInit{
     console.log('pause');
   }
 
+  restart() {
+    console.log('restart');
+  }
+
   reset() {
     console.log('reset');
-  }
-
-  save() {
-    console.log('save');
-  }
-
-  cancel() {
-    console.log('cancel');
   }
 
   formatTime(seconds: number): string {
     const hrs = Math.floor(seconds / 3600);
     const mins = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
-
     const hrsString = hrs < 10 ? '0' + hrs : hrs;
     const minsString = mins < 10 ? '0' + mins : mins;
     const secsString = secs < 10 ? '0' + secs : secs;
-
     return `${hrsString}:${minsString}:${secsString}`;
   }
 
@@ -76,11 +68,8 @@ export class LaboralTimerComponent implements OnInit{
     const now = new Date();
     const date = now.toISOString().split('T')[0]; // Fecha en formato YYYY-MM-DD
     const time = now.toTimeString().split(' ')[0]; // Hora en formato HH:MM:SS
-
     localStorage.setItem('start', `{date: ${date}, time: ${time}`);
   }
 
-
-
-
 }
+
